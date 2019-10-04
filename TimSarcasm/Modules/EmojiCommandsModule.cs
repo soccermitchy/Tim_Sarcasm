82db925a377e08f3d76358acaa2ac356837b55c7
Mitchell Monahan <me@mitchellmonahan.com>
2019-10-04 13:24:04 -0400

    🅱

---
+++ TimSarcasm/Modules/EmojiCommandsModule.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimSarcasm.Modules
+{
+
+    public class EmojiCommandsModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("b")]
+        [Summary("b")]
+        public async Task B()
+        {
+            await Context.Message.Channel.SendMessageAsync("🅱");
+        }
+    }
+}
