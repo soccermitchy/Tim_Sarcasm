6f3280e1865ad38a461c3e8043fca9e032661c1c
Mitchell Monahan <me@mitchellmonahan.com>
2019-10-05 22:01:29 -0400

    Create EventListenerAttribute (#7)

---
+++ TimSarcasm/EventListenerAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TimSarcasm
+{
+    public enum Event
+    {
+        UserVoiceStateUpdated
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class EventListenerAttribute : Attribute
+    {
+        public Event Event { get; private set; }
+        public EventListenerAttribute(Event ev) {
+            Event = ev;
+        }
+    }
+}
